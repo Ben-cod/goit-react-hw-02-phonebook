1bd800b66e7d6683901eb48e69f38f4c63b93832
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-08-14 22:25:13 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,18 +19,27 @@ export class App extends Component {
     filter: '',
   };
 
-  handleAddContact = (values) => {
+  handleAddContact = ({name, phone}) => {
     const newContact = {
       id: nanoid(),
-      name: values.name,
-      phone: values.phone,
+      name: name,
+      phone: phone,
     };
+
+    const isContactExists = this.state.contacts.some(
+      (contact) => contact.name === name || contact.phone === phone
+    );
+  
+    if (isContactExists) {
+      console.log('Контакт з таким ім\'ям або номером телефону вже існує.');
+      return;
+    }
 
     this.setState((prevState) => ({
       contacts: [...prevState.contacts, newContact],
     }));
   };
-  
+
   handleDeleteContact = (id) => {
     this.setState((prevState) => ({
       contacts: prevState.contacts.filter((contact) => contact.id !== id)
@@ -40,23 +49,23 @@ export class App extends Component {
     this.setState({ filter: event.target.value });
   }
 
-  render(){
-    const {contacts, filter} = this.state
+  render() {
+    const { contacts, filter } = this.state
 
     const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
+      contact.name.toLowerCase().includes(filter.toLowerCase())
 
-  );
+    );
     return (
       <Container>
-        
+
         <ContactForm onSubmit={this.handleAddContact} />
         <h2>Contacts</h2>
-        <Filter value={this.filter} onChange={this.handleFilterChange}/>
-        <ContactList contacts={filteredContacts} onDelete={this.handleDeleteContact}/>
+        <Filter value={this.filter} onChange={this.handleFilterChange} />
+        <ContactList contacts={filteredContacts} onDelete={this.handleDeleteContact} />
       </Container>
-     );
+    );
 
   }
-  
+
 };
