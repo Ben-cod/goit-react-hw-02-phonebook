dc98c75b7ea4232e829cba70d944bdfc7bc36f77
Ben-cod <123839625+Ben-cod@users.noreply.github.com>
2023-08-14 22:51:01 +0300

    add

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -4,7 +4,7 @@ import css from './Filter.module.css'
 export const Filter = ({value, onChange}) => (
     <div >
     <label className={css.filterWrap} htmlFor="filter"></label>
-    <input type="text" id="filter" placeholder="Find contacts by name" value={value} onChange={onChange} />
+    <input className={css.filter} type="text" id="filter" placeholder="Find contacts by name" value={value} onChange={onChange} />
   </div>
 )
    
